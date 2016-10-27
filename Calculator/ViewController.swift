bb678139d92732a6d3085bf546aa061e5aaa4716
Vasyl Melnychuk <vmmelnychuk@gmail.com>
2016-10-27 23:04:39 +0200

    add all together

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Vasyl Melnychuk on 10/27/16.
+//  Copyright © 2016 Magenta Margin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+}
+
